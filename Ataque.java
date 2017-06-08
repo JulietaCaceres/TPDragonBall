179efc14a43a648ae81837e0911bd3fedc09e1a4
JulietaCaceres <julieta.agustina.caceres@gmail.com>
2017-06-08 00:09:53 -0300

    Acá va la clase Ataque

---
+++ Ataque.java
@@ -0,0 +1,26 @@
+/**
+ * Created by july on 07/06/17.
+ */
+public class Ataque {
+
+    public  void atacar(Personaje atacante, Personaje  oponente , Tablero tablero,double poderDeAtaque, int distanciaAtaque )
+    {
+        valildarEquipo(atacante, oponente);
+        validarDistancia(atacante, oponente, tablero, distanciaAtaque);
+        oponente.recibirDaño(poderDeAtaque);
+    }
+
+    public void validarEquipo(Personaje atacante, Personaje oponente)
+    {
+        if(atacante.obtenerEquipo != oponente.obtenerEquipo)
+            throw new SonDelMismoEquipoException();
+    }
+
+    public void  validarDistancia(Personaje atacante, Personaje oponente, Tablero tablero, int distanciaAtaque)
+    {
+        int [] casilleroAtacante = tablero.obtenerCordenadasDe(atacante);
+        int [] casilleroOponente = tablero.obtenerCoordenadasDe(oponente);
+        if ( (distanciaAtaque <= Math.abs(casilleroOponente[0] - casilleroAtacante[0]) &&
+           (distanciaAtaque <= Math.abs(casilleroOponente[1] - casilleroAtacante[1]))
+            throw new laDistanciaNoEsValidaException();
+    }
