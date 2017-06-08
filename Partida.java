1ea812b56b69830d7fe587afb7c07391fe65ce22
JimeRosello <jimena.rosello@gmail.com>
2017-06-08 14:36:40 -0300

    Create Partida.java

---
+++ Partida.java
@@ -0,0 +1,18 @@
+
+public class Partida {
+
+	private Tablero tablero; 
+  private Equipo equipo1; 
+  private Equipo equipo2; 
+	
+	public Partida(string nombreEquipo1, string nombreEquipo2) { 
+      this.tablero = new Tablero(); 
+      this.equipo1 = new Equipo(nombreEquipo1); 
+      this.equipo2 = new Equipo(nombreEquipo2); 
+  }
+  
+  public Tablero obtenerTablero() { 
+      return this.tablero; 
+  } 
+	
+}
