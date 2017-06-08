6f48615e80205150986505f4fed9d647d1479ef1
JimeRosello <jimena.rosello@gmail.com>
2017-06-08 17:51:26 -0300

    Create Equipo.java

--- Equipo.java
+++ Equipo.java
@@ -1,9 +1,11 @@
 public class Equipo {
 
     private string nombre;
+    private string[] participantes; 
     
-    public Equipo(string nombreEquipo) { 
-        this.nombre = nombreEquipo;     
+    public Equipo(string[] participantes, string nombreEquipo) { 
+        this.nombre = nombreEquipo;
+        this.participantes = participantes; 
     } 
 
 }
