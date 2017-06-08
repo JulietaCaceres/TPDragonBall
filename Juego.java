e841ae719c9595a4f3c9c209c3a6dbc41e6dde2e
JimeRosello <jimena.rosello@gmail.com>
2017-06-08 14:25:13 -0300

    Create Juego.java

---
+++ Juego.java
@@ -0,0 +1,13 @@
+public class Juego {
+	
+	private Menu menuDeJuego; 
+	
+	public Juego(Menu menuDeJuego) { 
+		this.menuDeJuego = menuDeJuego; 
+	}
+	
+	public Menu obtenerMenu() { 
+		return this.menuDeJuego; 
+	}
+	
+}
