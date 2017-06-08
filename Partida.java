96661220ae504fb6724538b68ddaf97ed8a3fe05
JulietaCaceres <julieta.agustina.caceres@gmail.com>
2017-06-08 19:04:19 -0300

    Add files via upload

--- Partida.java
+++ Partida.java
@@ -1,15 +1,15 @@
+import Juego.Tablero;
 public class Partida {
-	
-	int campo [] = {48,27};
-	
+
+
     public void crearEquipos(){
-    	string guerreros [] = {"Goku", "Gohan", "Piccolo"};
-    	string enemigos [] = {"Cell", "Freezer", "MajinBoo"};
-    	Equipo guerrerosZ = new GuerrerosZ (guerreros);
-    	Equipo enemigosDeLaTierra = new EnemigosDeLaTierra (enemigos);
+        String guerreros [] = {"Goku", "Gohan", "Piccolo"};
+        String enemigos [] = {"Cell", "Freezer", "MajinBoo"};
+        Equipo guerrerosZ = new GuerrerosZ (guerreros);
+        Equipo enemigosDeLaTierra = new EnemigosDeLaTierra (enemigos);
     }
-    
+
     public void crearTablero(){
-    	Tablero tablero = new Tablero (campo);
+        Tablero tablero = new Tablero ();
     }
 }
