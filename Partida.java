fe3bd3777b831201d45bac65bcdc27444e7574fd
JimeRosello <jimena.rosello@gmail.com>
2017-06-08 17:40:10 -0300

    Create Partida.java

--- Partida.java
+++ Partida.java
@@ -1,18 +1,15 @@
-
 public class Partida {
-
-    private Tablero tablero; 
-    private Equipo equipo1;
-    private Equipo equipo2; 
 	
-    public Partida(string nombreEquipo1, string nombreEquipo2) { 
-    this.tablero = new Tablero(); 
-    this.equipo1 = new Equipo(nombreEquipo1); 
-    this.equipo2 = new Equipo(nombreEquipo2); 
-  }
-  
-  public Tablero obtenerTablero() { 
-      return this.tablero; 
-  } 
-
+	int campo [] = {48,27};
+	
+    public void crearEquipos(){
+    	String guerreros [] = {"Goku", "Gohan", "Piccolo"};
+    	String enemigos [] = {"Cell", "Freezer", "MajinBoo"};
+    	Equipo guerrerosZ = new GuerrerosZ (guerreros);
+    	Equipo enemigosDeLaTierra = new EnemigosDeLaTierra (enemigos);
+    }
+    
+    public void crearTablero(){
+    	Tablero tablero = new Tablero (campo);
+    }
 }
