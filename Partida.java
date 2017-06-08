31d0c657c5d2d90b8de8c879331f76363f7d3896
JimeRosello <jimena.rosello@gmail.com>
2017-06-08 17:47:23 -0300

    Create Partida.java

--- Partida.java
+++ Partida.java
@@ -3,8 +3,8 @@ public class Partida {
 	int campo [] = {48,27};
 	
     public void crearEquipos(){
-    	String guerreros [] = {"Goku", "Gohan", "Piccolo"};
-    	String enemigos [] = {"Cell", "Freezer", "MajinBoo"};
+    	string guerreros [] = {"Goku", "Gohan", "Piccolo"};
+    	string enemigos [] = {"Cell", "Freezer", "MajinBoo"};
     	Equipo guerrerosZ = new GuerrerosZ (guerreros);
     	Equipo enemigosDeLaTierra = new EnemigosDeLaTierra (enemigos);
     }
