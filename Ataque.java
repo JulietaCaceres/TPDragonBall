041641213c04bf7efc94b9fb4a55415feb3c95d0
nazar9318 <fedelias93@gmail.com>
2017-06-08 10:50:01 -0300

    Update Ataque.java

--- Ataque.java
+++ Ataque.java
@@ -1,3 +1,5 @@
+package Juego;
+
 /**
  * Created by july on 07/06/17.
  */
@@ -5,22 +7,23 @@ public class Ataque {
 
     public  void atacar(Personaje atacante, Personaje  oponente , Tablero tablero,double poderDeAtaque, int distanciaAtaque )
     {
-        valildarEquipo(atacante, oponente);
-        validarDistancia(atacante, oponente, tablero, distanciaAtaque);
+        this.validarEquipo(atacante, oponente);
+        this.validarDistancia(atacante, oponente, tablero, distanciaAtaque);
         oponente.recibirDaño(poderDeAtaque);
     }
 
     public void validarEquipo(Personaje atacante, Personaje oponente)
     {
-        if(atacante.obtenerEquipo != oponente.obtenerEquipo)
+        if(atacante.obtenerEquipo() != oponente.obtenerEquipo())
             throw new SonDelMismoEquipoException();
     }
 
     public void  validarDistancia(Personaje atacante, Personaje oponente, Tablero tablero, int distanciaAtaque)
     {
-        int [] casilleroAtacante = tablero.obtenerCordenadasDe(atacante);
+        int [] casilleroAtacante = tablero.obtenerCoordenadasDe(atacante);
         int [] casilleroOponente = tablero.obtenerCoordenadasDe(oponente);
-        if ( (distanciaAtaque <= Math.abs(casilleroOponente[0] - casilleroAtacante[0]) &&
-           (distanciaAtaque <= Math.abs(casilleroOponente[1] - casilleroAtacante[1]))
+        if ( (distanciaAtaque <= Math.abs(casilleroOponente[0] - casilleroAtacante[0])) &&
+           (distanciaAtaque <= Math.abs(casilleroOponente[1] - casilleroAtacante[1])))
             throw new laDistanciaNoEsValidaException();
     }
+}
