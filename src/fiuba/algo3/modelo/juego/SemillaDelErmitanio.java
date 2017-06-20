885b23afdaddf011ec1df8e101028d8b20100cbd
JimeRosello <jimena.rosello@gmail.com>
2017-06-20 15:12:03 -0300

    quito override de las clases que implementan Consumible

--- src/fiuba/algo3/modelo/juego/SemillaDelErmitanio.java
+++ src/fiuba/algo3/modelo/juego/SemillaDelErmitanio.java
@@ -4,7 +4,6 @@ import fiuba.algo3.modelo.personajes.Personaje;
 
 public class SemillaDelErmitanio implements Consumible{
 
-	@Override
 	public void aplicarEfecto(Personaje personaje) {
 		personaje.comerSemillaDelErmitanio();
 	}
